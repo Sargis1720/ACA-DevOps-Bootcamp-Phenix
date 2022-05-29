7235914ef19c0ff2016c84880934b9668adbb44d
andsedrakyan <andranik.sedrakyan.2003@gmail.com>
2022-05-29 15:04:42 +0400

    Installation

--- script1.sh
+++ script1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 function check_if_installed() {
   check=$(which nginx)
-  
+
   if [[ $check != "/usr/sbin/nginx" ]]; then
     echo "You have not installed nginx"
     sudo apt -y update
     sudo apt-get -y install nginx
   else
-    echo "You have nginx installed already"
+    echo "You have nginx installed already!"
   fi
 }
 
