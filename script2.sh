16a285ad660be1e3cf3819592cb64712f3e86263
hoso11 <hovsep.07@mail.ru>
2022-05-29 17:28:47 +0400

    second script

--- script2.sh
+++ script2.sh
@@ -2,17 +2,17 @@
 
 
 #  download new defoult file
-sudo curl https://transfer.sh/QX7UuX/bootcamp_aca.conf --output /etc/nginx/sites-enabled/default1
+sudo curl https://transfer.sh/QX7UuX/bootcamp_aca.conf --output /etc/nginx/sites-enabled/default
 
 #   change directory 
-cd /etc/nginx/sites-enabled/ 
+#cd /etc/nginx/sites-enabled/ 
 
 #   remove old defoult file
-rm default
+#rm default
 
 #   rename new defoult file to defoule
 
-mv defoult1 defoult
+#mv defoult1 defoult
 
 #   reload nginix 
 
